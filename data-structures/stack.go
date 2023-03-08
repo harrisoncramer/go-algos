6012bdd23e1582a756df2c907787b9af8dd157bf
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-08 11:41:27 -0700

    Added stack

--- data-structures/stack.go
+++ data-structures/stack.go
@@ -1,1 +1,18 @@
 package datastructures
+
+import "errors"
+
+type Stack []int
+
+func (s *Stack) Push(v int) {
+	*s = append(*s, v)
+}
+
+func (s Stack) Pop() (int, error) {
+	length := len(s)
+	if length == 0 {
+		return 0, errors.New("This stack is empty")
+	}
+
+	return s[length-1], nil
+}
---
+++ data-structures/stack_test.go
@@ -0,0 +1,56 @@
+package datastructures
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	t.Run("Should add items to the stack", func(t *testing.T) {
+
+		stack := Stack{}
+		stack.Push(3)
+		stack.Push(4)
+
+		want := Stack{3, 4}
+
+		if !reflect.DeepEqual(stack, want) {
+			t.Errorf("Got %v but wanted %v", stack, want)
+		}
+
+	})
+
+	t.Run("Should remove an item from the stack", func(t *testing.T) {
+		stack := Stack{}
+		stack.Push(3)
+		stack.Push(2)
+		stack.Push(6)
+		stack.Push(3)
+		stack.Push(4)
+		got, err := stack.Pop()
+
+		if err != nil {
+			t.Errorf("Error when popping from stack: %v", err)
+		}
+
+		want := 4
+		if got != want {
+			t.Errorf("Got %d but wanted %d", got, want)
+		}
+
+	})
+
+	t.Run("Should error if no items and removing value", func(t *testing.T) {
+		stack := Stack{}
+		_, err := stack.Pop()
+
+		if err == nil {
+			t.Error("Did not receive error when popping from empty stack")
+		}
+
+		if err.Error() != "This stack is empty" {
+			t.Errorf("Incorrect message when popping from empty stack: %s", err.Error())
+		}
+
+	})
+}
