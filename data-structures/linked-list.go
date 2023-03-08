5f62ec88cc5d9e5b00baa5099218b9bf8b2f9fe3
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-08 13:51:45 -0700

    Added linked list

--- data-structures/linked-list.go
+++ data-structures/linked-list.go
@@ -1,1 +1,129 @@
 package datastructures
+
+import "errors"
+
+type Node struct {
+	val  int
+	next *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func (ll *LinkedList) Append(v int) {
+
+	newNode := &Node{
+		val:  v,
+		next: nil,
+	}
+
+	current := ll.head
+	if current == nil {
+		ll.head = newNode
+	} else {
+		for current.next != nil {
+			current = current.next
+		}
+
+		current.next = newNode
+	}
+}
+
+func (ll *LinkedList) Search(v int) int {
+	current := ll.head
+
+	var j int
+	for current != nil {
+		if current.val == v {
+			return j
+		}
+		j++
+		current = current.next
+	}
+
+	return -1
+}
+
+func (ll *LinkedList) Insert(v int, j int) error {
+	if ll.head == nil {
+		ll.Append(v)
+		return nil
+	}
+
+	if j == 0 {
+		temp := ll.head
+		ll.head = &Node{
+			val:  v,
+			next: temp,
+		}
+		return nil
+	}
+
+	current := ll.head.next
+	prev := ll.head
+
+	i := 0
+	for i < j && current != nil {
+		prev = current
+		current = current.next
+		i++
+	}
+
+	if i < j {
+		return errors.New("That index is out of bounds")
+	}
+
+	prev.next = &Node{
+		val:  v,
+		next: current,
+	}
+
+	return nil
+}
+
+func (ll *LinkedList) Delete(v int) error {
+	if ll.head == nil {
+		return errors.New("This list is empty")
+	}
+
+	/* Deleting head */
+	if ll.head.val == v {
+		ll.head = ll.head.next
+		return nil
+	}
+
+	current := ll.head
+	prev := ll.head
+
+	for current.val != v && current.next != nil {
+		prev = current
+		current = current.next
+	}
+
+	if current.next == nil {
+		/* We reached the end of the list */
+		if current.val == v {
+			prev.next = nil
+			return nil
+		} else {
+			return errors.New("That value does not exist")
+		}
+	} else {
+		/* We reached the node, rmeove  */
+		prev.next = current.next
+	}
+
+	return nil
+}
+
+func (ll *LinkedList) Traverse() []int {
+	current := ll.head
+	results := []int{}
+	for current != nil {
+		results = append(results, current.val)
+		current = current.next
+	}
+
+	return results
+}
---
+++ data-structures/linked-list_test.go
@@ -0,0 +1,173 @@
+package datastructures
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestLinkedList(t *testing.T) {
+	t.Run("Should add values to a linked list", func(t *testing.T) {
+		ll := LinkedList{}
+		ll.Append(5)
+		ll.Append(6)
+		ll.Append(7)
+
+		want := []int{5, 6, 7}
+		got := ll.Traverse()
+		if reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v but wanted %v", got, want)
+		}
+	})
+
+	t.Run("Should add value at specific index to a linked list", func(t *testing.T) {
+		ll := LinkedList{}
+		ll.Append(5)
+		ll.Append(7)
+		ll.Append(8)
+
+		ll.Insert(6, 1)
+
+		if ll.head.val != 5 {
+			t.Errorf("Got %d for head but wanted %d", ll.head.val, 5)
+		}
+
+		want := []int{5, 6, 7, 8}
+		got := ll.Traverse()
+		if reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v but wanted %v", got, want)
+		}
+
+	})
+
+	t.Run("Should 'insert' value at the beginning", func(t *testing.T) {
+		ll := LinkedList{}
+		ll.Append(5)
+		ll.Append(7)
+		ll.Append(8)
+
+		ll.Insert(4, 0)
+
+		want := []int{4, 5, 6, 7}
+		got := ll.Traverse()
+		if reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v but wanted %v", got, want)
+		}
+
+		if ll.head.val != 4 {
+			t.Errorf("Got %d for head but wanted %d", ll.head.val, 4)
+		}
+	})
+
+	t.Run("Should error when adding value out of bounds", func(t *testing.T) {
+		ll := LinkedList{}
+		ll.Append(4)
+		ll.Append(5)
+		ll.Append(6)
+		ll.Append(7)
+
+		err := ll.Insert(8, 5)
+
+		if err.Error() != "That index is out of bounds" {
+			t.Error("Should have errored with out of bounds insertion")
+		}
+
+		want := []int{4, 5, 6, 7}
+		got := ll.Traverse()
+		if reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v but wanted %v", got, want)
+		}
+
+	})
+
+	t.Run("Should get the index of a value in the linked list", func(t *testing.T) {
+		ll := LinkedList{}
+		ll.Append(4)
+		ll.Append(5)
+		ll.Append(6)
+		ll.Append(7)
+
+		got := ll.Search(6)
+		want := 2
+		if got != want {
+			t.Errorf("Got %d but wanted %d", got, want)
+		}
+
+	})
+
+	t.Run("Should return -1 for a non-existent value", func(t *testing.T) {
+		ll := LinkedList{}
+		ll.Append(4)
+		ll.Append(5)
+		ll.Append(6)
+		ll.Append(7)
+
+		got := ll.Search(9)
+		want := -1
+		if got != want {
+			t.Errorf("Got %d but wanted %d", got, want)
+		}
+	})
+
+	t.Run("Should delete the head", func(t *testing.T) {
+		ll := LinkedList{}
+		ll.Append(4)
+		ll.Append(5)
+		ll.Append(6)
+
+		ll.Delete(4)
+
+		got := ll.Traverse()
+		want := []int{5, 6}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v but wanted %v", got, want)
+		}
+	})
+
+	t.Run("Should delete a value from the middle", func(t *testing.T) {
+		ll := LinkedList{}
+		ll.Append(4)
+		ll.Append(5)
+		ll.Append(6)
+
+		ll.Delete(5)
+		got := ll.Traverse()
+		want := []int{4, 6}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v but wanted %v", got, want)
+		}
+
+	})
+
+	t.Run("Should delete the tail", func(t *testing.T) {
+		ll := LinkedList{}
+		ll.Append(4)
+		ll.Append(5)
+		ll.Append(6)
+
+		ll.Delete(6)
+		got := ll.Traverse()
+		want := []int{4, 5}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v but wanted %v", got, want)
+		}
+	})
+
+	t.Run("Should error if deleting non-existent value", func(t *testing.T) {
+		ll := LinkedList{}
+		ll.Append(4)
+		ll.Append(5)
+		ll.Append(6)
+
+		err := ll.Delete(9)
+		if err == nil {
+			t.Errorf("Did not error")
+		}
+
+		got := ll.Traverse()
+		want := []int{4, 5, 6}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v but wanted %v", got, want)
+		}
+
+	})
+}
