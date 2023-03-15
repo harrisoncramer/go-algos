a9e16f5fa2bc2778d917044bf5d33ca3142d3927
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-15 10:55:09 -0600

    Added basic Add + DFS operations to binary tree

--- data-structures/binarytree.go
+++ data-structures/binarytree.go
@@ -1,1 +1,34 @@
 package datastructures
+
+type BinaryTree struct {
+	value int
+	left  *BinaryTree
+	right *BinaryTree
+}
+
+func (n *BinaryTree) Add(v int) *BinaryTree {
+	if n == nil {
+		return &BinaryTree{v, nil, nil}
+	} else if v <= n.value {
+		n.left = n.left.Add(v)
+	} else {
+		n.right = n.right.Add(v)
+	}
+	return n
+}
+
+func (tree *BinaryTree) DepthFirstSearchPrint(result []int) []int {
+
+	if tree.left != nil {
+		result = tree.left.DepthFirstSearchPrint(result)
+	}
+
+	result = append(result, tree.value)
+
+	if tree.right != nil {
+		result = tree.right.DepthFirstSearchPrint(result)
+	}
+
+	return result
+
+}
--- data-structures/binarytree_test.go
+++ data-structures/binarytree_test.go
@@ -1,1 +1,28 @@
 package datastructures
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBinaryTree(t *testing.T) {
+	t.Run("Should build a binary tree", func(t *testing.T) {
+
+		tree := BinaryTree{
+			value: 5,
+		}
+
+		tree.Add(12)
+		tree.Add(2)
+		tree.Add(10)
+		tree.Add(1)
+		tree.Add(3)
+
+		got := tree.DepthFirstSearchPrint([]int{})
+		want := []int{1, 2, 3, 5, 10, 12}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v but wanted %v", got, want)
+		}
+	})
+}
