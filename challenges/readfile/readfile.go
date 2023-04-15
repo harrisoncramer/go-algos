0a803ad1a38fa580ecbccf5de4cc312628395379
Harrison Cramer <harrisoncramer@gmail.com>
2023-04-15 18:18:01 -0400

    Added readfile functions

---
+++ challenges/readfile/readfile.go
@@ -0,0 +1,62 @@
+package readfile
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+/* Write a function that reads from a file and captures its output and prints it to the screen */
+func ReadFile(filepath string) error {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return errors.New("Could not open file: " + err.Error())
+	}
+
+	defer func() {
+		if cerr := file.Close(); err != nil {
+			fmt.Println("Failed to close file: " + cerr.Error())
+		}
+	}()
+
+	defer file.Close()
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		return errors.New("Could not read file: " + err.Error())
+	}
+
+	log.Printf("%s", bytes)
+	return nil
+}
+
+/* Write a function that reads a file using the bufio package to handle large files or streaming from an input */
+func ReadLarge(filepath string) error {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return errors.New("Could not open file: " + err.Error())
+	}
+
+	defer file.Close()
+
+	newReader := bufio.NewReader(file)
+
+	for {
+		delim := byte('\n')
+		s, err := newReader.ReadSlice(delim)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return errors.New("Could not read file: " + err.Error())
+			}
+		} else {
+			fmt.Printf("%s", s)
+		}
+	}
+
+	return nil
+}
